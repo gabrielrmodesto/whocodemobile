bf671a2120716e2732238ec032c65ef37aba54fe
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-01-19 17:58:52 -0300

    Config routes and status bar

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,33 @@
 import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigaiton-stack";
+import { createStackNavigator } from "react-navigation-stack";
 import Main from "./pages/Main";
 import Profile from "./pages/Profile";
 
 const Routes = createAppContainer(
-  createStackNavigator({
-    Main,
-    Profile
-  })
+  createStackNavigator(
+    {
+      Main: {
+        screen: Main,
+        navigationOptions: {
+          title: "Who Code"
+        }
+      },
+      Profile: {
+        screen: Profile,
+        navigationOptions: {
+          title: "Github Profile"
+        }
+      }
+    },
+    {
+      defaultNavigationOptions: {
+        headerTintColor: "#FFF",
+        headerStyle: {
+          backgroundColor: "#7D40E7"
+        }
+      }
+    }
+  )
 );
 
 export default Routes;
