dc8fc79c210c01bb82c1face535394fe8cbd12c9
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-01-19 18:17:12 -0300

    Take user position

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,8 +1,39 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StyleSheet } from "react-native";
+import MapView from "react-native-maps";
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 export default function Main() {
-  return (
-    <View />
-  );
+	const [currentRegion, setCurrentRegion] = useState(null);
+
+	useEffect(() => {
+		async function loadInitialPosition() {
+			const { granted } = await requestPermissionsAsync();
+			if(granted){
+				const { coords } = await getCurrentPositionAsync({
+					enableHighAccuracy: true,
+				});
+				const { latitude, longitude } = coords;
+
+				setCurrentRegion({
+					latitude,
+					longitude,
+					latitudeDelta: 0.04,
+					longitudeDelta: 0.04,
+				})
+			}
+		}
+		loadInitialPosition();
+	}, []);
+
+	if(!currentRegion){
+		return null;
+	}
+	return <MapView style={styles.map} />;
 }
+
+const styles = StyleSheet.create({
+	map: {
+		flex: 1
+	}
+});
